a86cbfba893184c1ff5fb08054baf01ee87072fa
Aziza <aziza.temiralieva1@gmail.com>
2022-12-09 17:15:27 +0600

    Timer

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
-
+# =====
 check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
